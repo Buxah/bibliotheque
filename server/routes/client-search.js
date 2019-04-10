fc09b20679c64d7e89e9daabe74d93ccad47cf1a
Lucas Dominique <lucasdominique99@gmail.com>
2019-04-10 11:39:21 +0200

    Update client-search.js

--- server/routes/client-search.js
+++ server/routes/client-search.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/search/:title', function(req, res, next) {
+router.get('/', function(req, res, next) {
     var title = req.params.title;
     Product.find({title: title}, function (err, products) {
         if(err) {
-            return res.render('shop/search', {products: null});
+            return res.render('client/search', {products: null});
 
         }
-        res.render('shop/search', {products: products});
+        res.render('clients/search', {products: products});
     });
 });
